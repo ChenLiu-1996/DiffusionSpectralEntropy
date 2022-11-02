05339e4ac068eec0352cad9f73326e353bda301f
Chen Liu <cl3760@columbia.edu>
2022-11-02 14:28:56 -0400

    minor edits to investigate.py. Nothing interesting yet.

--- src/investigate.py
+++ src/investigate.py
@@ -1,66 +1,81 @@
 import argparse
-import os
-import sys
 from typing import List
 
 import numpy as np
 import torch
+from barlowtwins_model import BarlowTwinsModel
 from gtda.diagrams import PersistenceEntropy
 from gtda.homology import VietorisRipsPersistence
 from matplotlib import pyplot as plt
+from moco_model import MoCoModel
+from simsiam_model import SimSiamModel
+from swav_model import SwavModel
 from tqdm import tqdm
 
-sys.path.insert(0, os.path.abspath('./'))
-from moco_model import MoCoModel
 
-
-def extract_features(model_name: str,
-                     load_weights: bool = True,
+def extract_features(load_weights: bool = True,
                      noise_list: List[torch.Tensor] = None,
                      device: torch.device = None):
 
     __models = ['barlowtwins', 'moco', 'simsiam', 'swav']
-    assert model_name in __models
-
     __versions = {
+        'barlowtwins': ['barlowtwins_bs2048_ep1000'],
         'moco': ['moco_v1_ep200', 'moco_v2_ep200', 'moco_v2_ep800'],
+        'simsiam': ['simsiam_bs256_ep100', 'simsiam_bs512_ep100'],
+        'swav': [
+            'swav_bs256_ep200', 'swav_bs256_ep400',
+            'swav_bs4096_ep100', 'swav_bs4096_ep200',
+            'swav_bs4096_ep400', 'swav_bs4096_ep800',
+        ],
     }
     top1_acc = {
+        'barlowtwins': [73.5],
         'moco': [60.6, 67.7, 71.1],
+        'simsiam': [68.3, 68.1],
+        'swav': [72.7, 74.3, 72.1, 73.9, 74.6, 75.3],
     }
     summary = {}
 
-    for i, version in enumerate(__versions[model_name]):
-        summary[version] = {
-            'manifold_features': None,
-            'top1_acc': top1_acc[model_name][i],
-        }
+    for model_name in __models:
+        for i, version in enumerate(__versions[model_name]):
+            summary[version] = {
+                'manifold_features': None,
+                'top1_acc': top1_acc[model_name][i],
+            }
 
-        if model_name == 'moco':
-            model = MoCoModel(device=device, version=version)
+            if model_name == 'barlowtwins':
+                model = BarlowTwinsModel(device=device, version=version)
+            elif model_name == 'moco':
+                model = MoCoModel(device=device, version=version)
+            elif model_name == 'simsiam':
+                model = SimSiamModel(device=device, version=version)
+            elif model_name == 'swav':
+                model = SwavModel(device=device, version=version)
+            else:
+                raise ValueError('model_name: %s not supported.' % model_name)
 
-        if load_weights:
-            model.restore_model()
-        model.eval()
+            if load_weights:
+                model.restore_model()
+            model.eval()
 
-        embeddings = []
-        with torch.no_grad():
-            for noise in tqdm(noise_list):
-                _ = model.forward(noise)
-                embedding_dict = model.fetch_latent()
-                embeddings.append(
-                    embedding_dict['avgpool'].cpu().detach().numpy().squeeze(0))
-        embeddings = np.array(embeddings)
+            embeddings = []
+            with torch.no_grad():
+                for noise in tqdm(noise_list):
+                    _ = model.forward(noise)
+                    embedding_dict = model.fetch_latent()
+                    embeddings.append(
+                        embedding_dict['avgpool'].cpu().detach().numpy().squeeze(0))
+            embeddings = np.array(embeddings)
 
-        embeddings = embeddings.squeeze()
+            embeddings = embeddings.squeeze()
 
-        VR = VietorisRipsPersistence(homology_dimensions=[0, 1, 2])
-        point_clouds = embeddings[None, ...]
-        diagrams = VR.fit_transform(point_clouds)
-        PE = PersistenceEntropy()
-        features = PE.fit_transform(diagrams)
+            VR = VietorisRipsPersistence(homology_dimensions=[0, 1])
+            point_clouds = embeddings[None, ...]
+            diagrams = VR.fit_transform(point_clouds)
+            PE = PersistenceEntropy()
+            features = PE.fit_transform(diagrams)
 
-        summary[version]['manifold_features'] = features
+            summary[version]['manifold_features'] = features
 
     return summary
 
@@ -82,8 +97,21 @@ if __name__ == '__main__':
     # Convert to a list of tensors with B = 1.
     noise_list = [noises[i, ...][None, ...] for i in range(noises.shape[0])]
 
-    summary = extract_features(
-        model_name='moco', noise_list=noise_list, device=device)
+    summary = extract_features(noise_list=noise_list, device=device)
 
-    import pdb
-    pdb.set_trace()
+    points = []
+    colors = []
+    candidates = []
+    for candidate in summary.keys():
+        candidates.append(candidate)
+        feature_acc_dict = summary[candidate]
+        points.append(feature_acc_dict['manifold_features'])
+        colors.append(feature_acc_dict['top1_acc'])
+    points = np.array(points)
+    points = points.squeeze(1)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    sc = ax.scatter(points[:, 0], points[:, 1], c=colors, alpha=0.8)
+    plt.colorbar(sc)
+    fig.savefig('manifold_features.png')
