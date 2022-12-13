0642d6075f22289cc9c313c5593e1b4696d5be0b
Chen Liu <chliu@hephaestus.cs.yale.edu>
2022-12-13 18:51:19 -0500

    In progress of writing the CPSC code

--- CPSC644-toy-study/train_simple_model.py
+++ CPSC644-toy-study/train_simple_model.py
@@ -29,49 +29,94 @@ def get_dataloaders(
     if config.dataset == 'mnist':
         config.in_channels = 1
         config.num_classes = 10
-        transform = torchvision.transforms.Compose([
+
+        transform_train = torchvision.transforms.Compose([
             torchvision.transforms.RandomCrop(32, padding=4),
             torchvision.transforms.RandomHorizontalFlip(),
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize((0.1307, ), (0.3081, ))
         ])
+        transform_val = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.1307, ), (0.3081, ))
+        ])
+
         train_loader = torch.utils.data.DataLoader(
             torchvision.datasets.MNIST(config.dataset_dir,
                                        train=True,
                                        download=True,
-                                       transform=transform),
+                                       transform=transform_train),
             batch_size=config.batch_size,
             shuffle=True)
         validation_loader = torch.utils.data.DataLoader(
             torchvision.datasets.MNIST(config.dataset_dir,
                                        train=False,
                                        download=True,
-                                       transform=transform),
+                                       transform=transform_val),
             batch_size=config.batch_size,
             shuffle=False)
 
     elif config.dataset == 'cifar10':
         config.in_channels = 3
         config.num_classes = 10
-        transform = torchvision.transforms.Compose([
+
+        transform_train = torchvision.transforms.Compose([
             torchvision.transforms.RandomCrop(32, padding=4),
             torchvision.transforms.RandomHorizontalFlip(),
             torchvision.transforms.ToTensor(),
             torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465),
                                              (0.2023, 0.1994, 0.2010)),
         ])
+        transform_val = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                             (0.2023, 0.1994, 0.2010)),
+        ])
+
         train_loader = torch.utils.data.DataLoader(
             torchvision.datasets.CIFAR10(config.dataset_dir,
                                          train=True,
                                          download=True,
-                                         transform=transform),
+                                         transform=transform_train),
             batch_size=config.batch_size,
             shuffle=True)
         validation_loader = torch.utils.data.DataLoader(
             torchvision.datasets.CIFAR10(config.dataset_dir,
                                          train=False,
                                          download=True,
-                                         transform=transform),
+                                         transform=transform_val),
+            batch_size=config.batch_size,
+            shuffle=False)
+
+    elif config.dataset == 'cifar100':
+        config.in_channels = 3
+        config.num_classes = 100
+
+        transform_train = torchvision.transforms.Compose([
+            torchvision.transforms.RandomCrop(32, padding=4),
+            torchvision.transforms.RandomHorizontalFlip(),
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                             (0.2023, 0.1994, 0.2010)),
+        ])
+        transform_val = torchvision.transforms.Compose([
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.4914, 0.4822, 0.4465),
+                                             (0.2023, 0.1994, 0.2010)),
+        ])
+
+        train_loader = torch.utils.data.DataLoader(
+            torchvision.datasets.CIFAR100(config.dataset_dir,
+                                          train=True,
+                                          download=True,
+                                          transform=transform_train),
+            batch_size=config.batch_size,
+            shuffle=True)
+        validation_loader = torch.utils.data.DataLoader(
+            torchvision.datasets.CIFAR100(config.dataset_dir,
+                                          train=False,
+                                          download=True,
+                                          transform=transform_val),
             batch_size=config.batch_size,
             shuffle=False)
 
@@ -97,10 +142,18 @@ def train(config: AttributeHashmap) -> None:
     opt = torch.optim.AdamW(model.parameters(),
                             lr=float(config.learning_rate),
                             weight_decay=float(config.weight_decay))
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(opt, T_max=200)
 
     train_loader, val_loader = dataloaders
 
     loss_fn_classification = torch.nn.CrossEntropyLoss()
+
+    is_model_saved = {
+        'val_acc_50%': False,
+        'val_acc_70%': False,
+    }
+    best_val_acc = 0
+    best_model = None
 
     for _ in tqdm(range(config.max_epoch)):
         state_dict = {
@@ -110,25 +163,25 @@ def train(config: AttributeHashmap) -> None:
             'val_acc': 0,
         }
 
+        model.train()
+        correct, total = 0, 0
         for x, y_true in train_loader:
+            B = x.shape[0]
             assert config.in_channels in [1, 3]
             if config.in_channels == 1:
                 # Repeat the channel dimension: 1 channel -> 3 channels.
                 x = x.repeat(1, 3, 1, 1)
 
-            x = x.to(device)
-            y_true = y_true.to(device)
+            x, y_true = x.to(device), y_true.to(device)
 
-            model.train()
             if config.contrastive == 'NA':
                 # Not using contrastive learning.
                 y_pred = model(x)
                 loss = loss_fn_classification(y_pred, y_true)
-                state_dict['train_loss'] += loss.item() / len(train_loader)
-                import pdb
-                pdb.set_trace()
-                state_dict['train_acc'] += torch.argmax(
-                    y_pred) == y_true / len(train_loader)
+                state_dict['train_loss'] += loss.item() * B
+                correct += torch.sum(
+                    torch.argmax(y_pred, dim=-1) == y_true).item()
+                total += B
 
                 opt.zero_grad()
                 loss.backward()
@@ -136,29 +189,63 @@ def train(config: AttributeHashmap) -> None:
 
             elif config.contrastive == 'simclr':
                 # Using SimCLR.
+                # x_aug1 = augment(x, random_seed=config.random_seed)
+                # x_aug2 = augment(x, random_seed=config.random_seed)
                 raise NotImplementedError
 
+        state_dict['train_loss'] /= total
+        state_dict['train_acc'] = correct / total * 100
+
+        model.eval()
         for x, y_true in val_loader:
+            B = x.shape[0]
             assert config.in_channels in [1, 3]
             if config.in_channels == 1:
                 # Repeat the channel dimension: 1 channel -> 3 channels.
                 x = x.repeat(1, 3, 1, 1)
 
-            x = x.to(device)
-            y_true = y_true.to(device)
+            x, y_true = x.to(device), y_true.to(device)
 
-            model.eval()
             if config.contrastive == 'NA':
                 # Not using contrastive learning.
                 y_pred = model(x)
                 loss = loss_fn_classification(y_pred, y_true)
-                state_dict['val_loss'] += loss.item() / len(val_loader)
+                state_dict['val_loss'] += loss.item() * B
+                correct += torch.sum(
+                    torch.argmax(y_pred, dim=-1) == y_true).item()
+                total += B
 
             elif config.contrastive == 'simclr':
                 # Using SimCLR.
                 raise NotImplementedError
 
+        state_dict['val_loss'] /= total
+        state_dict['val_acc'] = correct / total * 100
+
+        scheduler.step()
         print(state_dict)
+
+        if state_dict['val_acc'] > best_val_acc:
+            best_model = model.state_dict()
+            model_save_path = '%s/%s-%s-%s' % (
+                config.checkpoint_dir, config.dataset, config.contrastive,
+                'best_val_acc.pth')
+            os.makedirs(os.path.dirname(model_save_path), exist_ok=True)
+            torch.save(best_model, model_save_path)
+
+            if state_dict['val_acc'] > 50 and not is_model_saved['val_acc_50%']:
+                model_save_path = '%s/%s-%s-%s' % (
+                    config.checkpoint_dir, config.dataset, config.contrastive,
+                    'val_acc_50%.pth')
+                torch.save(best_model, model_save_path)
+                is_model_saved['val_acc_50%'] = True
+
+            if state_dict['val_acc'] > 70 and not is_model_saved['val_acc_70%']:
+                model_save_path = '%s/%s-%s-%s' % (
+                    config.checkpoint_dir, config.dataset, config.contrastive,
+                    'val_acc_70%.pth')
+                torch.save(best_model, model_save_path)
+                is_model_saved['val_acc_70%'] = True
 
 
 if __name__ == '__main__':
