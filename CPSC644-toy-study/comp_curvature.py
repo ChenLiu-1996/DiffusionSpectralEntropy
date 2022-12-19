a8d78937c9ffb858631e90267587ade6ea53756a
Danqi Liao <danqiliao@vpn172022116036.its.yale.internal>
2022-12-19 18:04:13 -0500

    plugin embed curv

--- CPSC644-toy-study/comp_curvature.py
+++ CPSC644-toy-study/comp_curvature.py
@@ -4,6 +4,7 @@ Compute local curvature using diffusion curvature for input embeddings
 '''
 import numpy as np
 import pandas as pd
+from glob import glob
 
 from diffusion_curvature.core import DiffusionMatrix
 from diffusion_curvature.laziness import curvature
@@ -18,10 +19,10 @@ def comp_curvature(X, labels, n_classes, data_df, k=20, diffusion_powers=8):
 
     # Curvature
     diffusion_curvatures = curvature(P, diffusion_powers=diffusion_powers)
-    # data_df['curvature'] = diffusion_curvatures
+    data_df['curvature'] = diffusion_curvatures
 
     # Compute curvature related stats per class
-    for ci in n_classes:
+    for ci in range(n_classes):
         c_rows = data_df.loc[data_df['class'] == ci]
         curvs = c_rows['curvature']
 
@@ -34,8 +35,22 @@ def comp_curvature(X, labels, n_classes, data_df, k=20, diffusion_powers=8):
 
 
 if __name__ == '__main__':
-    X = np.random.rand(100,50) # N x dim
+    #X = np.random.rand(100,50) # N x dim
+    files = sorted(glob('./results/embeddings/mnist-NA-val_acc_70%/*'))
+    batch_0_file = np.load(files[0])
 
-    curvature, class_stats = comp_curvature(X, [], [], [])
-    data_df['curvature'] = curvature
+    image = batch_0_file['image']
+    label = batch_0_file['label_true']
+    embeddings = batch_0_file['embedding']
+
+    print(image.shape)
+    print(label.shape)
+    print(embeddings.shape)
+
+    N,D = embeddings.shape
+    df = pd.DataFrame(embeddings, columns=[f'd{i}' for i in range(0, D)])
+    df['class'] = label
+
+    curvature, class_stats = comp_curvature(embeddings, label, n_classes=np.max(label)+1, data_df=df)
+
     print(np.mean(curvature))
