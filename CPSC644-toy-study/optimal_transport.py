e43001911456f1a9b4d75dde9a837acd8fd1cafd
Danqi Liao <danqiliao@vpn17225174243.its.yale.internal>
2022-12-19 22:52:23 -0500

    test op

--- CPSC644-toy-study/optimal_transport.py
+++ CPSC644-toy-study/optimal_transport.py
@@ -7,10 +7,13 @@ import pandas as pd
 import scipy
 import networkx as nx
 import ot
+from glob import glob
 
-def comp_geodesic(X, labels, n_classes, data_df, k=20, diffusion_powers=8):
+
+def comp_geodesic(X, k=20):
     '''
         X: (N, D)
+        geodesic: (N, N)
     '''
     # Compute Geodesic
     N,D = X.shape
@@ -38,24 +41,49 @@ def comp_geodesic(X, labels, n_classes, data_df, k=20, diffusion_powers=8):
             geodesic[i, j] = int(path_lens[i][j])
     print('Geodesci finished ... ', geodesic.shape)
 
-    #
-
     return geodesic
 
-def comp_op(geodesic):
+def comp_op(a, b, geodesic):
     '''
-    geodesic: (N,N) cost matrix
+    geodesic: (N,n) cost matrix
     '''
     # a and b are 1D histograms (sum to 1 and positive)
     # M is the ground cost matrix
     T = ot.emd(a, b, geodesic)  # exact linear program
     total_cost = np.sum(T*geodesic)
 
+    return total_cost
+
+
+def comp_opcost(X, labels, n_classes, geodesic):
+    N,D = X.shape
+    opc = np.zeros((N,N))
+
+    # Compute op cost marix
+    for si in range(n_classes):
+        sindex = list(np.squeeze(np.argwhere(labels==si)))
+        for ti in range(n_classes):
+            tindex = list(np.squeeze(np.argwhere(labels==ti)))
+            cost_mat = geodesic[sindex,:][:, tindex]
+            cost = comp_op(1/len(sindex), 1/len(tindex), cost_mat)
+
+            opc[si,ti] = cost
+
+    return opc
 
 if __name__ == '__main__':
-    X = np.random.rand(100,50) # N x dim
+    files = sorted(glob('./results/embeddings/mnist-NA-val_acc_70%/*'))
+    batch_0_file = np.load(files[0])
 
-    curvature, class_stats = comp_geodesic(X, [], [], [])
+    image = batch_0_file['image']
+    label = batch_0_file['label_true']
+    embeddings = batch_0_file['embedding']
 
-    data_df['curvature'] = curvature
-    print(np.mean(curvature))
+    print(image.shape)
+    print(label.shape)
+    print(embeddings.shape)
+
+    geodesic = comp_geodesic(embeddings, k=20)
+    opc = comp_opcost(embeddings, label, n_classes=np.max(label)+1, geodesic=geodesic)
+
+    print(opc.shape)
