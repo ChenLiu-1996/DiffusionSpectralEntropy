61ff468ba248d7b60609dbac3f20c67aa05ab448
Chen Liu <cl3760@columbia.edu>
2022-12-31 19:40:28 -0500

    added code to save per-class acc in inference

--- CPSC644-toy-study/train_infer.py
+++ CPSC644-toy-study/train_infer.py
@@ -136,14 +136,16 @@ def train(config: AttributeHashmap) -> None:
     log(config_str, filepath=log_path, to_console=False)
 
     model = ResNet50(num_classes=config.num_classes).to(device)
-    opt = torch.optim.Adam(model.parameters(),
-                           lr=float(config.learning_rate),
-                           weight_decay=float(config.weight_decay))
-    lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=opt,
-                                                              mode='max',
-                                                              factor=1 / 5,
-                                                              patience=5,
-                                                              verbose=True)
+    opt = torch.optim.AdamW(model.parameters(),
+                            lr=float(config.learning_rate),
+                            weight_decay=float(config.weight_decay))
+    # lr_scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=opt,
+    #                                                           mode='max',
+    #                                                           factor=1 / 5,
+    #                                                           patience=5,
+    #                                                           verbose=True)
+    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(
+        optimizer=opt, T_max=config.max_epoch)
 
     loss_fn_classification = torch.nn.CrossEntropyLoss()
     loss_fn_simclr = NTXentLoss()
@@ -205,11 +207,11 @@ def train(config: AttributeHashmap) -> None:
                 x_aug1, x_aug2, y_true = x_aug1.to(device), x_aug2.to(
                     device), y_true.to(device)
 
-                if batch_idx < 0.8 * len(train_loader):
+                if batch_idx < 0.95 * len(train_loader):
                     # Freeze linear classifier, train encoder.
                     if not simclr_stage1_initialized:
-                        model.freeze_linear()
                         model.unfreeze_encoder()
+                        model.freeze_linear()
                         simclr_stage1_initialized = True
 
                     z1 = model.encode(x_aug1)
@@ -231,15 +233,19 @@ def train(config: AttributeHashmap) -> None:
                     # Freeze encoder, train linear classifier.
                     if not simclr_stage2_initialized:
                         model.init_linear()
-                        model.unfreeze_linear()
                         model.freeze_encoder()
+                        model.unfreeze_linear()
                         simclr_stage2_initialized = True
 
-                    y_pred = model(x_aug1)
-                    loss = loss_fn_classification(y_pred, y_true)
+                    y_pred_aug1, y_pred_aug2 = model(x_aug1), model(x_aug2)
+                    loss_aug1 = loss_fn_classification(y_pred_aug1, y_true)
+                    loss_aug2 = loss_fn_classification(y_pred_aug2, y_true)
+                    loss = (loss_aug1 + loss_aug2) / 2
                     correct += torch.sum(
-                        torch.argmax(y_pred, dim=-1) == y_true).item()
-                    total += B
+                        torch.argmax(y_pred_aug1, dim=-1) == y_true).item()
+                    correct += torch.sum(
+                        torch.argmax(y_pred_aug2, dim=-1) == y_true).item()
+                    total += 2 * B
 
                     opt.zero_grad()
                     loss.backward()
@@ -296,7 +302,7 @@ def train(config: AttributeHashmap) -> None:
             best_model = model.state_dict()
             model_save_path = '%s/%s-%s-%s' % (
                 config.checkpoint_dir, config.dataset, config.contrastive,
-                'best_val_acc.pth')
+                'val_acc_best.pth')
             torch.save(best_model, model_save_path)
             log('Best model (so far) successfully saved.',
                 filepath=log_path,
@@ -338,18 +344,22 @@ def infer(config: AttributeHashmap) -> None:
 
     model = ResNet50(num_classes=config.num_classes).to(device)
 
-    checkpoint_paths = glob(
-        '%s/%s-%s*.pth' %
-        (config.checkpoint_dir, config.dataset, config.contrastive))
+    checkpoint_paths = sorted(
+        glob('%s/%s-%s*.pth' %
+             (config.checkpoint_dir, config.dataset, config.contrastive)))
+    log_path = '%s/%s-%s.log' % (config.log_dir, config.dataset,
+                                 config.contrastive)
 
     for checkpoint in tqdm(checkpoint_paths):
         checkpoint_name = checkpoint.split('/')[-1].replace('.pth', '')
         model.load_state_dict(torch.load(checkpoint))
         model.eval()
 
+        total_by_class = {}
+        correct_by_class = {}
+
         with torch.no_grad():
             for batch_idx, (x, y_true) in enumerate(val_loader):
-                B = x.shape[0]
                 assert config.in_channels in [1, 3]
                 if config.in_channels == 1:
                     # Repeat the channel dimension: 1 channel -> 3 channels.
@@ -357,6 +367,22 @@ def infer(config: AttributeHashmap) -> None:
                 x, y_true = x.to(device), y_true.to(device)
 
                 z = model.encode(x)
+                y_pred = model(x)
+                y_correct = (torch.argmax(
+                    y_pred, dim=-1) == y_true).cpu().detach().numpy()
+
+                # Record per-class accuracy.
+                for i in range(y_true.shape[0]):
+                    class_true = y_true[i].item()
+                    is_correct = y_correct[i]
+                    if class_true not in total_by_class.keys():
+                        total_by_class[class_true] = 1
+                        assert class_true not in correct_by_class.keys()
+                        correct_by_class[class_true] = 0
+                    else:
+                        total_by_class[class_true] += 1
+                    if is_correct:
+                        correct_by_class[class_true] += 1
 
                 save_numpy(config=config,
                            batch_idx=batch_idx,
@@ -364,6 +390,14 @@ def infer(config: AttributeHashmap) -> None:
                            image_batch=x,
                            label_true_batch=y_true,
                            embedding_batch=z)
+
+            log('\nCheckpoint: %s' % checkpoint_name,
+                filepath=log_path,
+                to_console=True)
+            log('#total by class:', filepath=log_path, to_console=True)
+            log(str(total_by_class), filepath=log_path, to_console=True)
+            log('#correct by class:', filepath=log_path, to_console=True)
+            log(str(correct_by_class), filepath=log_path, to_console=True)
 
     return
 
