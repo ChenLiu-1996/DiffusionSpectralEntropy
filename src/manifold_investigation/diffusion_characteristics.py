dbf09759468db7904a634fb2c7abc22dfbbf6264
Chen Liu <cl3760@columbia.edu>
2023-03-06 12:31:38 -0500

    updated diffusion_characteristics for mor plotting

--- src/manifold_investigation/diffusion_characteristics.py
+++ src/manifold_investigation/diffusion_characteristics.py
@@ -39,21 +39,26 @@ cifar10_int2name = {
 }
 
 
-def von_neumann_entropy(data, k: int = 1):
+def von_neumann_entropy(data, trivial_thr: float = 0.9):
     from scipy.linalg import svd
     _, eigenvalues, _ = svd(data)
 
-    eigenvalues = sorted(eigenvalues)
-    # Drop the top k biggest eigenvalues.
-    eigenvalues = eigenvalues[k:]
+    eigenvalues = np.array(sorted(eigenvalues)[::-1])
+    # Drop the biggest eigenvalue(s).
+    eigenvalues = eigenvalues[eigenvalues < trivial_thr]
 
-    prob = eigenvalues / np.sum(eigenvalues)
+    prob = eigenvalues / eigenvalues.sum()
     prob = prob + np.finfo(float).eps
 
     return -np.sum(prob * np.log(prob))
 
 
-def get_laplacian_extrema(data, n_extrema, knn=10, n_pca=100, subsample=True):
+def get_laplacian_extrema(data,
+                          n_extrema,
+                          knn=10,
+                          n_pca=100,
+                          subsample=True,
+                          random_state=0):
     '''
     Finds the 'Laplacian extrema' of a dataset.  The first extrema is chosen as
     the point that minimizes the first non-trivial eigenvalue of the Laplacian graph
@@ -66,6 +71,7 @@ def get_laplacian_extrema(data, n_extrema, knn=10, n_pca=100, subsample=True):
     import scipy
 
     if subsample and data.shape[0] > 10000:
+        np.random.seed(random_state)
         data = data[np.random.choice(data.shape[0], 10000, replace=False), :]
     G = gt.Graph(data, use_pygsp=True, decay=None, knn=knn, n_pca=n_pca)
 
@@ -111,6 +117,11 @@ if __name__ == '__main__':
     parser.add_argument('--config',
                         help='Path to config yaml file.',
                         required=True)
+    parser.add_argument('-P',
+                        '--plot-diffusion-eigenvalues',
+                        help='Plot diffusion eigenvalues distribution.',
+                        action='store_true')
+    parser.add_argument('--knn', help='k for knn graph.', type=int, default=10)
     args = vars(parser.parse_args())
     args = AttributeHashmap(args)
 
@@ -124,20 +135,30 @@ if __name__ == '__main__':
 
     save_root = './diffusion_PHATE/'
     os.makedirs(save_root, exist_ok=True)
-    save_path_1 = '%s/extrema-PHATE-%s-%s.png' % (save_root, config.dataset,
-                                                  config.contrastive)
-    save_path_2 = '%s/extrema-dist-PHATE-%s-%s.png' % (
-        save_root, config.dataset, config.contrastive)
-    log_path = '%s/extrema-PHATE-%s-%s.txt' % (save_root, config.dataset,
-                                               config.contrastive)
+    save_path_fig0 = '%s/diffusion-eigenvalues-%s-%s-knn-%s.png' % (
+        save_root, config.dataset, config.contrastive, args.knn)
+    save_path_fig1 = '%s/extrema-PHATE-%s-%s-knn-%s.png' % (
+        save_root, config.dataset, config.contrastive, args.knn)
+    save_path_fig2 = '%s/extrema-dist-PHATE-%s-%s-knn-%s.png' % (
+        save_root, config.dataset, config.contrastive, args.knn)
+    save_path_fig3 = '%s/von-Neumann-%s-%s-knn-%s.png' % (
+        save_root, config.dataset, config.contrastive, args.knn)
+    log_path = '%s/log-%s-%s-knn-%s.txt' % (save_root, config.dataset,
+                                            config.contrastive, args.knn)
 
     num_rows = len(embedding_folders)
+    fig0 = plt.figure(figsize=(8, 6 * num_rows))
     fig1 = plt.figure(figsize=(8, 5 * num_rows))
     fig2 = plt.figure(figsize=(8, 5 * num_rows))
+    fig3 = plt.figure(figsize=(8, 5))
+    von_neumann_thr_list = [0.5, 0.7, 0.9, 0.95, 0.99]
+    vne_stats = {}
+    vne_x_axis_text, vne_x_axis_value = [], []
 
     for i, embedding_folder in enumerate(embedding_folders):
         files = sorted(glob(embedding_folder + '/*'))
-        log(os.path.basename(embedding_folder), log_path)
+        checkpoint_name = os.path.basename(embedding_folder)
+        log(checkpoint_name, log_path)
 
         labels, embeddings = None, None
 
@@ -169,21 +190,65 @@ if __name__ == '__main__':
         phate_op = phate.PHATE(random_state=0,
                                n_jobs=1,
                                n_components=2,
+                               knn=args.knn,
                                verbose=False)
         data_phate = phate_op.fit_transform(embeddings)
 
         #
         '''von Neumann Entropy'''
-        t = phate_op._find_optimal_t(t_max=100, plot=False, ax=None)
-        vne_ref = phate_op._von_neumann_entropy(t_max=t)[1][0]
-        vne = von_neumann_entropy(phate_op.diff_op)
-        print('phate vne: ', vne_ref, 'our vne: ', vne)
-        log('von Neumann Entropy: %.4f' % vne, log_path)
+        # t = phate_op._find_optimal_t(t_max=100, plot=False, ax=None)
+        # vne_ref = phate_op._von_neumann_entropy(t_max=t)[1][0]
+        log('von Neumann Entropy: ', log_path)
+        for trivial_thr in von_neumann_thr_list:
+            vne = von_neumann_entropy(phate_op.diff_op,
+                                      trivial_thr=trivial_thr)
+            log(
+                '    removing eigenvalues > %.2f: entropy = %.4f' %
+                (trivial_thr, vne), log_path)
+
+            if trivial_thr not in vne_stats.keys():
+                vne_stats[trivial_thr] = [vne]
+            else:
+                vne_stats[trivial_thr].append(vne)
+
+        vne_x_axis_text.append(checkpoint_name.split('_')[-1])
+        if '%' in vne_x_axis_text[-1]:
+            vne_x_axis_value.append(
+                int(vne_x_axis_text[-1].split('%')[0]) / 100)
+        else:
+            vne_x_axis_value.append(vne_x_axis_value[-1] + 0.1)
+
+        #
+        '''Diffusion map'''
+        if args.plot_diffusion_eigenvalues:
+            save_path_eigenvalues = '%s/numpy_files/diffusion-eigenvalues-%s-%s-knn-%s-%s.npz' % (
+                save_root, config.dataset, config.contrastive, args.knn,
+                checkpoint_name.split('_')[-1])
+            os.makedirs(os.path.dirname(save_path_eigenvalues), exist_ok=True)
+            if os.path.exists(save_path_eigenvalues):
+                # Load the eigenvalues if exists.
+                data_numpy = np.load(save_path_eigenvalues)
+                eigenvalues = data_numpy['eigenvalues']
+            else:
+                P = phate_op.graph.diff_op  # SPARSE instead of DENSE
+                eigenvalues, _ = np.linalg.eig(P.toarray())
+                # eigenvalues, eigenvectors = sparse.linalg.eigs(P, k=100)
+                with open(save_path_eigenvalues, 'wb+') as f:
+                    np.savez(f, eigenvalues=eigenvalues)
+            ax0 = fig0.add_subplot(2 * num_rows, 1, 2 * i + 1)
+            ax0.set_title(checkpoint_name)
+            ax0.hist(eigenvalues, color='w', edgecolor='k')
+            ax0 = fig0.add_subplot(2 * num_rows, 1, 2 * i + 2)
+            sns.boxplot(x=eigenvalues, color='skyblue', ax=ax0)
+            fig0.tight_layout()
+            fig0.savefig(save_path_fig0)
 
         #
         '''Laplacian Extrema'''
-        k = 10
-        extrema_inds = get_laplacian_extrema(data=embeddings, n_extrema=k)
+        n_extrema = 10
+        extrema_inds = get_laplacian_extrema(data=embeddings,
+                                             n_extrema=n_extrema,
+                                             knn=args.knn)
 
         ax = fig1.add_subplot(num_rows, 1, i + 1)
         colors = np.empty((N), dtype=object)
@@ -200,8 +265,7 @@ if __name__ == '__main__':
         scprep.plot.scatter2d(data_phate,
                               c=colors,
                               cmap=cmap,
-                              title='%s  von Neumann Entropy: %.3f' %
-                              (os.path.basename(embedding_folder), vne),
+                              title='%s' % checkpoint_name,
                               legend_anchor=(1.25, 1),
                               ax=ax,
                               xticks=False,
@@ -211,7 +275,7 @@ if __name__ == '__main__':
                               s=sizes)
 
         fig1.tight_layout()
-        fig1.savefig(save_path_1)
+        fig1.savefig(save_path_fig1)
 
         extrema = embeddings[extrema_inds]
         dist_matrix = pairwise_distances(extrema)
@@ -225,9 +289,24 @@ if __name__ == '__main__':
         ax = fig2.add_subplot(num_rows, 1, i + 1)
         sns.heatmap(dist_matrix, ax=ax)
         ax.set_title('%s  Extrema Euc distance: %.2f \u00B1 %.2f' %
-                     (os.path.basename(embedding_folder), dist_mean, dist_std))
+                     (checkpoint_name, dist_mean, dist_std))
         log('Extrema Euc distance: %.2f \u00B1 %.2f\n' % (dist_mean, dist_std),
             log_path)
 
         fig2.tight_layout()
-        fig2.savefig(save_path_2)
+        fig2.savefig(save_path_fig2)
+
+    ax = fig3.add_subplot(1, 1, 1)
+    for trivial_thr in von_neumann_thr_list:
+        ax.scatter(vne_x_axis_value, vne_stats[trivial_thr])
+    ax.legend(von_neumann_thr_list, bbox_to_anchor=(1.12, 0.4))
+    ax.set_xticks(vne_x_axis_value)
+    ax.set_xticklabels(vne_x_axis_text)
+    ax.set_title(
+        'von Neumann Entropy (at different eigenvalue removal threshold)')
+    ax.spines[['right', 'top']].set_visible(False)
+    # Plot separately to avoid legend mismatch.
+    for trivial_thr in von_neumann_thr_list:
+        ax.plot(vne_x_axis_value, vne_stats[trivial_thr])
+    fig3.tight_layout()
+    fig3.savefig(save_path_fig3)
