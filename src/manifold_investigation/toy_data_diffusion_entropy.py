a50dd497976a950ff7e6162fed9969b7d83e0fdb
Chen Liu <chen.liu.cl2482@yale.edu>
2023-05-14 21:30:03 -0400

    updated toy data vne

--- src/manifold_investigation/toy_data_diffusion_entropy.py
+++ src/manifold_investigation/toy_data_diffusion_entropy.py
@@ -18,38 +18,37 @@ import_dir = '/'.join(os.path.realpath(__file__).split('/')[:-2])
 sys.path.insert(0, import_dir + '/utils/')
 sys.path.insert(0, import_dir + '/embedding_preparation')
 from attribute_hashmap import AttributeHashmap
-from information import von_neumann_entropy, approx_eigvals, exact_eigvals
+from information import von_neumann_entropy, exact_eigvals
 from diffusion import compute_diffusion_matrix
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--random-seed', type=int, default=1)
-    parser.add_argument('--knn', help='k for knn graph.', type=int, default=10)
     args = vars(parser.parse_args())
     args = AttributeHashmap(args)
 
     save_root = './results_toy_data/'
     os.makedirs(save_root, exist_ok=True)
 
-    save_path_fig = '%s/toy-data-knn%s.png' % (save_root, args.knn)
+    save_path_fig = '%s/toy-data-VNE.png' % (save_root)
 
     plt.rcParams['font.family'] = 'serif'
     plt.rcParams['legend.fontsize'] = 15
     N = 500
-    D = 512
-    num_dim = 4
-    num_repetition = 2
+    D = 1024
+    num_dim = 20
+    num_repetition = 5
     matrix_mix_sizes = [400, 420, 440, 460, 480, 500]
-    approx_thr_list = [None, 1e-5, 1e-4, 1e-3, 3e-3, 1e-2]
+    noise_level_list = [0, 5e-2, 1e-1, 2e-1, 3e-1, 5e-1]
 
     alpha_list = np.linspace(0, 1, num_dim)
     dim_list = np.linspace(D // num_dim, D, num_dim, dtype=np.int16)
     vne_list_matrix = [[[] for _ in range(num_repetition)]
                        for _ in matrix_mix_sizes]
     vne_list_uniform = [[[] for _ in range(num_repetition)]
-                        for _ in range(len(approx_thr_list) + 1)]
+                        for _ in range(len(noise_level_list))]
     vne_list_gaussian = [[[] for _ in range(num_repetition)]
-                         for _ in range(len(approx_thr_list) + 1)]
+                         for _ in range(len(noise_level_list))]
 
     for i in range(num_repetition):
         for j, size in enumerate(matrix_mix_sizes):
@@ -62,33 +61,28 @@ if __name__ == '__main__':
                 vne_list_matrix[j][i].append(vne)
 
         for dim in tqdm(dim_list):
-            # Uniform distribution over [-1, 1] with distribution dimension == dim.
-            embeddings = np.random.uniform(-1, 1, size=(N, D))
-            if dim < D:
-                embeddings[:, dim:] = np.random.randn(1)
-            diffusion_matrix = compute_diffusion_matrix(embeddings)
-            eigenvalues_P = exact_eigvals(diffusion_matrix)
-            vne = von_neumann_entropy(eigenvalues_P)
-            vne_list_uniform[0][i].append(vne)
-            for j, thr in enumerate(approx_thr_list):
-                eigenvalues_P_approx = approx_eigvals(diffusion_matrix,
-                                                      filter_thr=thr)
-                vne_approx = von_neumann_entropy(eigenvalues_P_approx)
-                vne_list_uniform[j + 1][i].append(vne_approx)
+            for j, noise_level in enumerate(noise_level_list):
+                # Uniform distribution over [-1, 1] with distribution dimension == dim.
+                embeddings = np.random.uniform(-1, 1, size=(N, D))
+                if dim < D:
+                    embeddings[:, dim:] = np.random.randn(1)
+                embeddings += noise_level * np.random.uniform(
+                    -1, 1, size=(N, D))
+                diffusion_matrix = compute_diffusion_matrix(embeddings)
+                eigenvalues_P = exact_eigvals(diffusion_matrix)
+                vne = von_neumann_entropy(eigenvalues_P)
+                vne_list_uniform[j][i].append(vne)
 
-            # Normal distribution with distribution dimension == dim.
-            embeddings = np.random.randn(N, D)
-            if dim < D:
-                embeddings[:, dim:] = np.random.randn(1)
-            diffusion_matrix = compute_diffusion_matrix(embeddings)
-            eigenvalues_P = exact_eigvals(diffusion_matrix)
-            vne = von_neumann_entropy(eigenvalues_P)
-            vne_list_gaussian[0][i].append(vne)
-            for j, thr in enumerate(approx_thr_list):
-                eigenvalues_P_approx = approx_eigvals(diffusion_matrix,
-                                                      filter_thr=thr)
-                vne_approx = von_neumann_entropy(eigenvalues_P_approx)
-                vne_list_gaussian[j + 1][i].append(vne_approx)
+                # Normal distribution with distribution dimension == dim.
+                embeddings = np.random.randn(N, D)
+                if dim < D:
+                    embeddings[:, dim:] = np.random.randn(1)
+                embeddings += noise_level * np.random.uniform(
+                    -1, 1, size=(N, D))
+                diffusion_matrix = compute_diffusion_matrix(embeddings)
+                eigenvalues_P = exact_eigvals(diffusion_matrix)
+                vne = von_neumann_entropy(eigenvalues_P)
+                vne_list_gaussian[j][i].append(vne)
 
     vne_list_matrix = np.array(vne_list_matrix)
     vne_list_uniform = np.array(vne_list_uniform)
@@ -105,8 +99,7 @@ if __name__ == '__main__':
             matrix_PD = datasets.make_spd_matrix(n_dim=matrix_dim,
                                                  random_state=9)
             mappable = ax.matshow(matrix_PD, cmap='Blues')
-            ax.set_xlabel(r'$n \times n$ Positive Definite Matrix',
-                          fontsize=20)
+            ax.set_xlabel(r'$n \times n$ Symmetric Matrix', fontsize=20)
             plt.colorbar(mappable, ax=ax, location='left', shrink=0.8)
         else:
             matrix_I = np.eye(matrix_dim)
@@ -192,80 +185,44 @@ if __name__ == '__main__':
                         cmap='tab10',
                         alpha=0.2)
     ax.tick_params(axis='both', which='major', labelsize=20)
-    ax.set_ylabel('Diffusion Entropy', fontsize=25)
+    ax.set_ylabel('Diffusion Spectral Entropy', fontsize=25)
     ax.set_xlabel(r'Weight Coefficient $\alpha$', fontsize=25)
 
-    ax = fig_vne.add_subplot(gs[1:2, 3:6])
+    ax = fig_vne.add_subplot(gs[1:, 3:6])
     ax.spines[['right', 'top']].set_visible(False)
-    ax.set_title('Full Eigendecomposition', fontsize=20)
-    ax.plot(dim_list,
-            np.mean(vne_list_uniform[0, ...], axis=0),
-            color='mediumblue')
-    ax.fill_between(dim_list,
-                    np.mean(vne_list_uniform[0, ...], axis=0) -
-                    np.std(vne_list_uniform[0, ...], axis=0),
-                    np.mean(vne_list_uniform[0, ...], axis=0) +
-                    np.std(vne_list_uniform[0, ...], axis=0),
-                    color='mediumblue',
-                    alpha=0.2)
-    ax.tick_params(axis='both', which='major', labelsize=20)
-
-    ax = fig_vne.add_subplot(gs[2:, 3:6])
-    ax.spines[['right', 'top']].set_visible(False)
-    ax.set_title('Chebyshev Approximation of Eigenvalues', fontsize=20)
-    for j, _ in enumerate(vne_list_uniform[1:]):
+    for j, _ in enumerate(noise_level_list):
         ax.plot(dim_list,
-                np.mean(vne_list_uniform[j + 1, ...], axis=0),
+                np.mean(vne_list_uniform[j, ...], axis=0),
                 color=cm.get_cmap('tab10').colors[j])
-    ax.legend([
-        r'$|P_{i,j}|$ > %s' % thr if thr is not None else r'full $P$'
-        for thr in approx_thr_list
-    ],
-              ncol=2)
-    for j in range(len(vne_list_uniform) - 1):
+    ax.legend(['|noise| = %d%%' % (item * 100) for item in noise_level_list],
+              loc='lower right',
+              ncol=3)
+    for j, _ in enumerate(noise_level_list):
         ax.fill_between(dim_list,
-                        np.mean(vne_list_uniform[j + 1, ...], axis=0) -
-                        np.std(vne_list_uniform[j + 1, ...], axis=0),
-                        np.mean(vne_list_uniform[j + 1, ...], axis=0) +
-                        np.std(vne_list_uniform[j + 1, ...], axis=0),
+                        np.mean(vne_list_uniform[j, ...], axis=0) -
+                        np.std(vne_list_uniform[j, ...], axis=0),
+                        np.mean(vne_list_uniform[j, ...], axis=0) +
+                        np.std(vne_list_uniform[j, ...], axis=0),
                         cmap='tab10',
                         alpha=0.2)
     ax.tick_params(axis='both', which='major', labelsize=20)
     ax.set_xlabel('Data Distribution Dimension $d$', fontsize=25)
 
-    ax = fig_vne.add_subplot(gs[1:2, 6:])
+    ax = fig_vne.add_subplot(gs[1:, 6:])
     ax.spines[['right', 'top']].set_visible(False)
-    ax.set_title('Full Eigendecomposition', fontsize=20)
-    ax.plot(dim_list,
-            np.mean(vne_list_gaussian[0, ...], axis=0),
-            color='mediumblue')
-    ax.fill_between(dim_list,
-                    np.mean(vne_list_gaussian[0, ...], axis=0) -
-                    np.std(vne_list_gaussian[0, ...], axis=0),
-                    np.mean(vne_list_gaussian[0, ...], axis=0) +
-                    np.std(vne_list_gaussian[0, ...], axis=0),
-                    color='mediumblue',
-                    alpha=0.2)
-    ax.tick_params(axis='both', which='major', labelsize=20)
-
-    ax = fig_vne.add_subplot(gs[2:, 6:])
-    ax.spines[['right', 'top']].set_visible(False)
-    ax.set_title('Chebyshev Approximation of Eigenvalues', fontsize=20)
-    for j, _ in enumerate(vne_list_gaussian[1:]):
+    for j, _ in enumerate(noise_level_list):
         ax.plot(dim_list,
-                np.mean(vne_list_gaussian[j + 1, ...], axis=0),
+                np.mean(vne_list_gaussian[j, ...], axis=0),
                 color=cm.get_cmap('tab10').colors[j])
-    ax.legend([
-        r'$|P_{i,j}|$ > %s' % thr if thr is not None else r'full $P$'
-        for thr in approx_thr_list
-    ],
-              ncol=2)
-    for j in range(len(vne_list_gaussian) - 1):
+    ax.legend(['|noise| = %d%%' % (item * 100) for item in noise_level_list],
+              loc='lower right',
+              ncol=3)
+    for j, _ in enumerate(noise_level_list):
         ax.fill_between(dim_list,
-                        np.mean(vne_list_gaussian[j + 1, ...], axis=0) -
-                        np.std(vne_list_gaussian[j + 1, ...], axis=0),
-                        np.mean(vne_list_gaussian[j + 1, ...], axis=0) +
-                        np.std(vne_list_gaussian[j + 1, ...], axis=0),
+                        np.mean(vne_list_gaussian[j, ...], axis=0) -
+                        np.std(vne_list_gaussian[j, ...], axis=0),
+                        np.mean(vne_list_gaussian[j, ...], axis=0) +
+                        np.std(vne_list_gaussian[j, ...], axis=0),
                         cmap='tab10',
                         alpha=0.2)
     ax.tick_params(axis='both', which='major', labelsize=20)
