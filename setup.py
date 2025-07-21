977bfebc4a0cb06bd2ff40c86612f957647c2bd9
Chen Liu <chen.liu.cl2482@yale.edu>
2025-07-21 14:57:17 -0400

    update readme

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='dse-dsmi',
-    version='1.0',
+    version='1.1',
     license='MIT',
     author='Chen Liu, Danqi Liao',
     author_email='chen.liu.cl2482@yale.edu',
