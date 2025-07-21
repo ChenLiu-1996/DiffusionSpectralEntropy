4dae490d8f3c3a33e124516ecc4b79421f698873
Chen Liu <chen.liu.cl2482@yale.edu>
2025-07-21 14:57:03 -0400

    update readme

--- dse_dsmi/__init__.py
+++ dse_dsmi/__init__.py
@@ -1,7 +1,10 @@
 # Copyright (c) 2025 Chen Liu, Danqi Liao
 # All rights reserved.
-from .dse import diffusion_spectral_entropy
-from .dsmi import diffusion_spectral_mutual_information
+from .dse import diffusion_spectral_entropy, adjacency_spectral_entropy
+from .dsmi import diffusion_spectral_mutual_information, adjacency_spectral_mutual_information
 
-__all__ = ['diffusion_spectral_entropy', 'diffusion_spectral_mutual_information']
+__all__ = ['diffusion_spectral_entropy',
+           'adjacency_spectral_entropy',
+           'diffusion_spectral_mutual_information',
+           'adjacency_spectral_mutual_information']
 
